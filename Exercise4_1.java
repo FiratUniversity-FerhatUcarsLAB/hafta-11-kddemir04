9bc13161261c648ef654d9e488b525382c628700
kddemir04 <240541007@firat.edu.tr>
2025-11-30 22:43:09 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,47 @@
+import java.util.Scanner;
+
 public class Exercise4_1 {
 
-    public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+    static void main() {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Day: ");
+        String gun = sc.nextLine();
+
+        System.out.println("Date: ");
+        int tarih = sc.nextInt();
+
+        sc.nextLine();
+
+        System.out.println("Month: ");
+        String ay = sc.nextLine();
+
+        System.out.println("Year: ");
+        int yil = sc.nextInt();
+
+        printAmerican(gun, tarih,ay,yil);
+        printEuropean(tarih, ay, yil, gun);
+
+        sc.close();
+
+
     }
 
-    public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+    public static void printAmerican(String day, int date,
+                                     String month, int year) {
+        System.out.println(day + ", " + month + " " + date + ", " + year);
+
+
     }
 
-    public static void main(String[] args) {
-        // TODO: test methods
+    public static void printEuropean(int date, String month,
+                                     int year, String day) {
+        System.out.println(date + " " + month + " " + year + ", " + day);
+
+
+
     }
 }
+
+
+
+
