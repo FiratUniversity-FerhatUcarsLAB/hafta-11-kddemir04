aa0ef67c8ca1a2149e77e32423e2f6577cabc603
kddemir04 <240541007@firat.edu.tr>
2025-12-01 23:33:39 +0300

    Update Exercise4_3.java

--- Exercise4_3.java
+++ Exercise4_3.java
@@ -1,3 +1,39 @@
+/*
+ * Ad Soyad: [ABDULKADİR DEMİR]
+ * Ogrenci No: [240541007]
+ * Tarih: [01.12.2025]
+ * Aciklama:4.3 tamamlandı
+ *
+ *
+ */
+
+
+//1)ping metodu ilk kez ¸ca˘grıldı˘gında programın durumunu g¨osteren bir stack diyagramı ¸cizin. (C¸a˘grı yı˘gını, aktif metotlar, parametreler ve yerel de˘gi¸skenler)
+/*answer) +---------------------------+
+| ping()                    |  ← Şu an çalışan metot
+| (parametre yok)           |
++---------------------------+
+| baffle()                  |  ← ping'i çağırdı
+| (parametre yok)           | 
+| local: yok                |
++---------------------------+
+| zoop()                    |  ← baffle'ı çağırdı
+| (parametre yok)           |
+| local: yok                |
++---------------------------+
+| main()                    |
+|parametre: (String[] args) |  ← program buradan başladı
+| local değişken: yok       |
++---------------------------+*/
+
+//2)A¸sa˘gıdaki programın ¨uretti˘gi tam ¸cıktıyı yazın. Bo¸sluklara, noktalara ve satır sonlarına dikkat edin.
+/*  No, I wug.
+    You wugga wug.
+    I wug.*/
+
+
+
+
 public class Exercise4_3 {
 
     public static void zoop() {
